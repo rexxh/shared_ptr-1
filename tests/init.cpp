7cc043e0f8e38da436284124e52991610dc5e589
hitode221 <marina.dr98@mail.ru>
2016-11-06 01:01:57 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,4 +4,37 @@
 SCENARIO("Init", "[Init]"){
   shared_ptr<int> a;
   REQUIRE(a.use_count() == 0);
+  REQUIRE(a.get() == nullptr);
+  REQUIRE(!a);
+}
+
+SCENARIO("with parameteres", "[with param]"){
+  shared_ptr<int> a(new int(5));
+  REQUIRE(a.use_count() == 1);
+  REQUIRE(*a == 5);
+  REQUIRE(a.unique());
+  REQUIRE(a);
+}
+
+SCENARIO("copy constructor", "[copy c]"){
+  shared_ptr<int> a(new int(5));
+  shared_ptr<int> b(a);
+  REQUIRE(*b == 5);
+  REQUIRE(b.use_count() == 2);
+}
+
+SCENARIO("move constructor", "[move c]"){
+  shared_ptr<int> a(new int(5));
+  shared_ptr<int> b(std::move(a));
+  REQUIRE(*b == 5);
+  REQUIRE(a.get() == nullptr);
+  REQUIRE(b.use_count() == 1);
+  REQUIE(a.use_count() == 0);  
+}
+
+SCENARIO("reset", "[reset]"){
+  shared_ptr<int> a(new int(5));
+  a.reset();
+  REQUIRE(a.use_count() == 0);
+  REQUIRE(a.get() == nullptr);
 }
