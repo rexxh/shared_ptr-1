dc95809a61bb1cd5d0271023b9081c1b1aca5bb7
rexxh <rexxh@mail.ru>
2016-12-05 09:29:31 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -3,24 +3,21 @@
 
 SCENARIO("Init", "[Init]"){
   shared_ptr<int> a;
-  REQUIRE(a.use_count() == 0);
+  REQUIRE(a.count() == 0);
   REQUIRE(a.get() == nullptr);
-  REQUIRE(!bool(a));
 }
 
 SCENARIO("with parameteres", "[with param]"){
   shared_ptr<int> a(new int(5));
-  REQUIRE(a.use_count() == 1);
+  REQUIRE(a.count() == 1);
   REQUIRE(*a == 5);
-  REQUIRE(a.unique());
-  //REQUIRE(a);
-}
+  }
 
 SCENARIO("copy constructor", "[copy c]"){
   shared_ptr<int> a(new int(5));
   shared_ptr<int> b(a);
   REQUIRE(*b == 5);
-  REQUIRE(b.use_count() == 2);
+  REQUIRE(b.count() == 2);
 }
 
 SCENARIO("copy operator", "[copy o]"){
@@ -28,7 +25,7 @@ SCENARIO("copy operator", "[copy o]"){
   shared_ptr<int> b;
   b = a;
   REQUIRE(*b == 5);
-  REQUIRE(b.use_count() == 2);
+  REQUIRE(b.count() == 2);
 }
 
 SCENARIO("move constructor", "[move c]"){
@@ -36,8 +33,8 @@ SCENARIO("move constructor", "[move c]"){
   shared_ptr<int> b(std::move(a));
   REQUIRE(*b == 5);
   REQUIRE(a.get() == nullptr);
-  REQUIRE(b.use_count() == 1);
-  REQUIRE(a.use_count() == 0);  
+  REQUIRE(b.count() == 1);
+  REQUIRE(a.count() == 0);  
 }
 
 SCENARIO("move operator", "[move o]"){
@@ -46,13 +43,13 @@ SCENARIO("move operator", "[move o]"){
   b = std::move(a);
   REQUIRE(*b == 5);
   REQUIRE(a.get() == nullptr);
-  REQUIRE(b.use_count() == 1);
-  REQUIRE(a.use_count() == 0);
+  REQUIRE(b.count() == 1);
+  REQUIRE(a.count() == 0);
 }
 
 SCENARIO("reset", "[reset]"){
   shared_ptr<int> a(new int(5));
   a.reset();
-  REQUIRE(a.use_count() == 0);
+  REQUIRE(a.count() == 0);
   REQUIRE(a.get() == nullptr);
 }
